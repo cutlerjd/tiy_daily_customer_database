d9ce3f356e03c80d12d944414df8909748f50790
Jase Cutler <jasecutler@gmail.com>
2017-07-12 14:42:54 -0700

    Foreach done and template

--- starter_files/main.js
+++ starter_files/main.js
@@ -1,1 +1,31 @@
-let arr_people = []
+let htmlBody = document.getElementsByTagName( "body" )[0];
+let wrapper = document.createElement( "div" );
+let header = document.createElement( "div" );
+let main = document.createElement( "div" );
+
+wrapper.classList.add("wrapper")
+header.classList.add("header")
+main.classList.add("main")
+
+
+
+let stringHTML = ""
+
+customers.results.forEach(function(item){
+  stringHTML += `
+  <div class="customer">
+    <img src="${item.picture.large}" alt="${item.name.first} ${item.name.last}" />
+    <h2 class="name">${item.name.first} ${item.name.last}</h2>
+    <div class="email">${item.email}</div>
+    <div class="addressLine1">${item.location.street}</div>
+    <div class="addressLine2">${item.location.city}, ${item.location.state} ${item.location.postcode}</div>
+    <div class="phone">${item.phone}</div>
+    <div class="ssn">${item.id.value}</div>
+  </div>
+  `
+})
+main.innerHTML = stringHTML;
+
+htmlBody.appendChild(wrapper)
+wrapper.appendChild(header)
+wrapper.appendChild(main)
